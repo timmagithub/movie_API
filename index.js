d2f495f03a50b6e196ae8036e754aa64eb7f2534
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-05-24 06:58:01 -0400

    Added content into the movie array.

--- index.js
+++ index.js
@@ -5,10 +5,71 @@ const app = express();
 
 app.use(morgan('common'));
 
-let topMovies = [];
+let topMovies = [
+    { 
+    'title': 'Fear and Loathing in Las Vegas',
+    'director': 'Terry Gilliam',
+    'year': '1998',
+    'genre': ['Comedy', 'Drama']
+},
+{ 
+    'title': 'The Matrix',
+    'director': 'The Wachowski Brothers',
+    'year': '1999',
+    'genre': ['Action', 'Thriller']
+},
+{ 
+    'title': 'There Will Ee Blood',
+    'director': 'Paul Thomas Anderson',
+    'year': '2007',
+    'genre': ['Drama', 'Thriller']
+},
+{ 
+    'title': 'Shaun of the Dead',
+    'director': 'Edgar Wright',
+    'year': '2004',
+    'genre': ['Comedy', 'Horror']
+},
+{ 
+    'title': 'Tropic Thunder',
+    'director': 'Ben Stiller',
+    'year': '2008',
+    'genre': ['Comedy', 'Action']
+},
+{ 
+    'title': 'Shutter Island',
+    'director': 'Martin Scorsese',
+    'year': '2010',
+    'genre': ['Thriller', 'Mystery']
+},
+{ 
+    'title': 'Raiders of the Lost Ark',
+    'director': 'Steven Spielberg',
+    'year': '1981',
+    'genre': ['Action', 'Adventure', 'Thriller']
+},
+{ 
+    'title': 'The Shining',
+    'director': 'Stanley Kubrick',
+    'year': '1980',
+    'genre': ['Horror', 'Suspense']
+},
+{ 
+    'title': 'The Proposition',
+    'director': 'John Hillcoat',
+    'year': '2005',
+    'genre': ''
+},
+{ 
+    'title': 'Birdman or (The Unexpected Virtue of Ignorance)',
+    'director': 'Alejandro G. Iñárritu',
+    'year': '2014',
+    'genre': ['comedy', 'drama']
+},
+];
 /*requests to navigate the site*/ 
 app.get('/movies', (req, res) => {
-    res.json(topMovies).send('Top 10 Movies');
+    res.json(topMovies);
 });
 
 app.get('/', (req, res) => {
