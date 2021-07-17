1ff0064c704025c037fcca3f39f6d3ba097069de
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-07-17 17:52:39 -0400

    Updated port to ready for launch

--- index.js
+++ index.js
@@ -229,11 +229,11 @@ app.use((err, req, res, next) => {
 
 
 
-// const port = process.env.PORT || 8080;
-// app.listen(port, '0.0.0.0',() => {
-//     console.log('Listening on Port ' + port);
-// });
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+    console.log('Listening on Port ' + port);
+});
 
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+// app.listen(8080, () => {
+//     console.log('Your app is listening on port 8080.');
+//   });
