07b28dd9085625ba1097bd1878d3bed092f2ea74
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-08-02 23:25:07 -0400

    remove jwt for client side build

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 });
 
 /* get list of all movies */
-app.get('/movies', passport.authenticate('jwt', {session:false}), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
